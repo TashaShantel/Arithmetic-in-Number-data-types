73d38c3e8cba194db6f8670dd4bd423e9e895caf
Natasha Onono <122837352+TashaShantel@users.noreply.github.com>
2023-02-23 13:11:01 +0000

    Arithmetic operations

--- main.dart
+++ main.dart
@@ -1,5 +1,13 @@
-void main () {
+  import 'dart:io';
 
+void main(){
   
+  print("Enter num1");
+  String num1 = stdin.readLineSync()!;
+
+  print("Enter num2");
+  String num2 = stdin.readLineSync()!;
   
+   print(int.parse(num1) + int.parse(num2)
+        );
 }
